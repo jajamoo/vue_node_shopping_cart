c74792726535fafe4e669571b89c0c8eeb996b91
Moe Kahn <ekahn@bluepallet.io>
2023-08-15 14:35:31 -0400

    Move Shopping cart into component

---
+++ frontend/src/components/ShoppingCartList.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div v-if="products.length > 0">
+    <div
+        class="product-container"
+        v-for="product in products"
+        :key="product.id"
+    >
+      <img class="product-image" :src="product.imageName" />
+      <div class="details-wrap">
+        <h3> {{ product.name }} </h3>
+        <p> {{ product.price }} </p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-else>
+    You currently have no items in your cart!
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "ShoppingCartList",
+    props: ['products']
+  }
+</script>
