3a2fbd9ddb3d27a85b32d05dc81f6d98c8f0ebd3
Moe Kahn <ekahn@bluepallet.io>
2023-08-15 10:54:33 -0400

    first commit

---
+++ vue_learn_frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App, yo!"/>-->
+  <h1>Olive Oil, Inc.</h1>
+  <div class="page-wrap">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {}
+};
+
+</script>
