1195c540938d61ef1328c86a530c261933d217d4
Moe Kahn <ekahn@bluepallet.io>
2023-08-15 11:07:51 -0400

    renamed frontend folder to frontend

--- frontend/src/pages/ShoppingCartPage.vue
+++ frontend/src/pages/ShoppingCartPage.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Shopping Cart Page</h1>
+
 </template>
 
 <script>
+import {cartItems} from "@/temp-data";
 export default {
   name : "ShoppingCartPage",
-  components: {}
+  data () {
+    return {
+      cartItems,
+    }
+  }
 }
 </script>
