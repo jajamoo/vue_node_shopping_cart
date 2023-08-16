d5ad74c271ab08ccc44c580cfcef0df66dccd257
Moe Kahn <ekahn@bluepallet.io>
2023-08-16 12:55:46 -0400

    mongo delete (CRUD) - DRY  async function

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,6 @@
 import express from 'express'
-import {cartItems as cartItemsRaw, products as productsRaw} from "./temp-data";
 import {MongoClient} from "mongodb";
 
-let cartItems = cartItemsRaw;
-let products = productsRaw;
 async function start() {
 
     const app = express();
@@ -75,23 +72,30 @@ async function start() {
             //no duplicates (vs $push, which doesn't account for duplicates)
             $addToSet: {cartItems: productId}
         });
+        
+        getUserAndPopulateCart(req, res);
+    });
 
-        // cartItems.push(productId);
+    app.delete('/users/:userId/cart/:productId', async (req, res) => {
+        const productId = req.params.productId;
+        const userId = req.params.userId;
+
+        await db.collection('users').updateOne({id: userId}, {
+            //no duplicates (vs $push, which doesn't account for duplicates)
+            $pull: {cartItems: productId}
+        });
+
+        getUserAndPopulateCart(req, res);
+    });
+
+    async function getUserAndPopulateCart(req, res){
         const user = await db.collection('users').findOne({
             id: req.params.userId
         });
         const populatedCart = await populateCartIds(user.cartItems);
 
         res.json(populatedCart);
-    });
-
-    app.delete('/cart/:productId', (req, res) => {
-        const productId = req.params.productId;
-
-        cartItems = cartItems.filter(id => id !== productId);
-        const populatedCart = populateCartIds(cartItems)
-        res.json(populatedCart);
-    });
+    }
 
     app.listen(8000, () => {
         console.log('Server listening on Port 8000')
